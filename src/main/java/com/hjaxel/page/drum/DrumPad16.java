38d4bf1bb0d891d5dbf0c1d890ec304a3f61bb56
Axel Hjälm <axelhjalm@gmail.com>
2017-09-17 20:38:12 +0200

    drum sequencer

--- src/main/java/com/hjaxel/page/drum/DrumPad16.java
+++ src/main/java/com/hjaxel/page/drum/DrumPad16.java
@@ -1,7 +1,10 @@
 package com.hjaxel.page.drum;
 
+import com.bitwig.extension.callback.StepDataChangedCallback;
 import com.bitwig.extension.controller.api.Clip;
 import com.bitwig.extension.controller.api.ControllerHost;
+import com.bitwig.extension.controller.api.PlayingNote;
+import com.bitwig.extension.controller.api.PlayingNoteArrayValue;
 import com.hjaxel.framework.MidiChannel;
 import com.hjaxel.framework.MidiChannelAndRange;
 import com.hjaxel.framework.MidiMessage;
@@ -15,6 +18,8 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 public class DrumPad16 extends MidiListener {
 
+    public static final int LOW_NOTE = 36;
+    public static final int HIGH_NOTE = 51;
     private final AtomicReference<DrumSequencer> selected = new AtomicReference<>();
     private final Map<Integer, DrumSequencer> pads = new HashMap<>();
     private final int[] encoderPadLookup = new int[]{48, 49, 50, 51, 44, 45, 46, 47, 40, 41, 42, 43, 36, 37, 38, 39};
@@ -24,12 +29,65 @@ public class DrumPad16 extends MidiListener {
             MidiChannelAndRange.of(MidiChannel.CHANNEL_1, 16, 31),
             MidiChannelAndRange.of(MidiChannel.CHANNEL_3, 16, 19)
     );
+    private final Clip clip;
 
     public DrumPad16(ControllerHost host, Clip clip) {
         super(host);
-        for (int i = 36; i <= 51; i++) {
+        this.clip = host.createLauncherCursorClip(127, 127);
+
+        clip.setIsSubscribed(true);
+
+        this.clip.playingStep().markInterested();
+        this.clip.addStepDataObserver((x, y, state) -> {
+            print("i got state");
+        });
+
+        this.clip.getPlayStart().markInterested();
+        this.clip.getPlayStop().markInterested();
+        this.clip.getLoopStart().markInterested();
+        this.clip.getLoopLength().markInterested();
+        this.clip.isLoopEnabled().markInterested();
+        this.clip.getShuffle().markInterested();
+        this.clip.getAccent().markInterested();
+        this.clip.canScrollStepsBackwards().markInterested();
+        this.clip.canScrollStepsForwards().markInterested();
+
+
+        PlayingNoteArrayValue playingNoteArrayValue = cursorTrack().playingNotes();
+        playingNoteArrayValue.markInterested();
+        playingNoteArrayValue.setIsSubscribed(true);
+        playingNoteArrayValue.addValueObserver(this::noteObserver);
+
+        enableObservers(true);
+
+        for (int i = LOW_NOTE; i <= HIGH_NOTE; i++) {
             pads.put(i, new DrumSequencer(host, clip, i));
         }
+    }
+
+    private void noteObserver(PlayingNote[] playingNotes) {
+        List<Integer> playing = new ArrayList<>();
+        for (PlayingNote playingNote : playingNotes) {
+            playing.add(playingNote.pitch());
+            sendValue(MidiChannel.CHANNEL_1, playingNote.pitch()-16, 127);
+        }
+
+        for(int i = 31; i < 51; i++){
+            if (!playing.contains(i)){
+                sendValue(MidiChannel.CHANNEL_1, i - 16, 80);
+            }
+        }
+    }
+
+    public void enableObservers(final boolean enable) {
+        this.clip.playingStep().setIsSubscribed(enable);
+        this.clip.getPlayStart().setIsSubscribed(enable);
+        this.clip.getPlayStop().setIsSubscribed(enable);
+        this.clip.getLoopStart().setIsSubscribed(enable);
+        this.clip.getLoopLength().setIsSubscribed(enable);
+        this.clip.isLoopEnabled().setIsSubscribed(enable);
+        this.clip.getShuffle().setIsSubscribed(enable);
+        this.clip.getAccent().setIsSubscribed(enable);
     }
 
     @Override
@@ -37,7 +95,6 @@ public class DrumPad16 extends MidiListener {
 
         for (MidiChannelAndRange midiChannelAndRange : observedMessages) {
             if (midiChannelAndRange.accepts(midiMessage)) {
-                print(this.getClass().getSimpleName() + " accepted " + midiMessage);
                 return handle(midiMessage);
             }
         }
